1346e9fb0404c5ca75049a8b07a4d4cdc5f2e02f
Mommel <mommel@gmx.de>
2021-02-03 20:08:54 +0100

    rm unneeded check

--- build/check_for_updates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SCRIPTFOLDER=$(dirname $0)
-cd $SCRIPTFOLDER
-cd ..
-outofdate=$(git status | grep Firmware)
-if ["${outofdate}" != ""] ; then
-  git submodule foreach --recursive git clean -f && git checkout . && git pull
-fi
