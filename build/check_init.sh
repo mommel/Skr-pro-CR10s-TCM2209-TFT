1b81220888860dc0f28c8d1b86c8755e7c8a7931
Mommel <mommel@gmx.de>
2021-03-21 17:41:15 +0100

    updates stack removes builder

--- build/check_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "**** Script started ==> $(pwd)/ >> ${0} ****"
-CHECK=$(npm ll | grep "UNMET DEPENDENCY")
-if [ "${CHECK}" != "" ]; then
-  npm install
-fi
-
-[ ! -d "node_modules" ] && npm install
-echo "**** ${0} DONE ****"
--- build/faileover_reinit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "**** Script started ==> $(pwd)/ >> ${0} ****"
-rm -rf "./node_modules"
-npm install
-echo "**** ${0} DONE ****"
